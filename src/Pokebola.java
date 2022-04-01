0a9e400374137f7303b53f3bc2651e42e3d88c7e
DiegoAleksis <diegoarh19@gmail.com>
2022-04-01 12:00:03 -0600

    InittialComit

--- src/Pokebola.java
+++ src/Pokebola.java
@@ -1,2 +1,29 @@
-public class Pokebola {
+public class Pokebola extends Objeto{
+    /* efectividad-double
+
+     */
+    private double efectividad;
+
+    public Pokebola(double costo, int cantidad, String nombre, String tipo, double efectividad) {
+        super(costo, cantidad, nombre, tipo);
+        this.efectividad = efectividad;
+    }
+
+    //atrapar retorna true/false
+    //tipo de pokebola
+        // siEs legendario -40 de efectividad
+            //hacemos un random del 1 al 100,
+            // si se obtiene un numero entre 1 y la efectividad
+            // que lo atrape
+            // y restamos pokebolas
+    //sino
+        //intenta otra vez, hasta que se acaben las pokebolas
+        // o el usuario ya no quiere intentar
+
+
+
+
+
+    //revisar el tipo de pokemon
+
 }
--- src/Poscion.java
+++ src/Poscion.java
@@ -1,2 +1,14 @@
-public class Poscion {
+public class Poscion extends Objeto{
+
+    /* efecto- String
+    duracion - entero
+            usar
+                si es curacion
+                    aumentar 20 hp
+                si es fuerza
+                    aumentar 20 de ataque
+                    si es velocidad
+                        aumentar 20 velocidad
+
+     */
 }
