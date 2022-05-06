bb5677424da524925b22a9e885003ecf7ae7890f
DiegoAleksis <diegoarh19@gmail.com>
2022-05-06 14:02:21 -0500

    Getters, setters, avance3

--- src/Tienda.java
+++ src/Tienda.java
@@ -7,23 +7,48 @@ public class Tienda {
     //objetos disponibles-ArrayList<Objetos>
 
     private String nombre;
-    private ArrayList<Objeto>objetosDisponibles;
-
-
+    private ArrayList<Objeto> objetosDisponibles;
 
 
     //metodo
 
-    public void comprar(double dinero, int cantidad, Objeto objeto ){
+    public boolean comprar(double dinero, int cantidad, Objeto objeto) {
         //validar que hay sufucientes objetos del tipo requerido (¿?)
         //cantidad y objeto
         //Si si tengo, validar que el dinero sea suficiente
         //Sino
         //
+
+        return false;
     }
+
     //comprar
     //vender
+    public boolean UsuarioVender(double dinero, int cantidad, Objeto objeto) {
 
 
+        return false;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Objeto> getObjetosDisponibles() {
+        return objetosDisponibles;
+    }
+
+    public void setObjetosDisponibles(ArrayList<Objeto> objetosDisponibles) {
+        this.objetosDisponibles = objetosDisponibles;
+    }
+
+    public Tienda(String nombre, ArrayList<Objeto> objetosDisponibles) {
+        this.nombre = nombre;
+        this.objetosDisponibles = objetosDisponibles;
+    }
 }
+
